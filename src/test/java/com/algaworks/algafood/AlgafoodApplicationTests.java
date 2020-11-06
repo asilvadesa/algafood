deca52942ccf8b6cbf26c949ef773458b73a21fd
Anderson de Sa <asilvadesa@gmail.com>
2020-11-05 22:44:15 -0400

    Adicionando JPA e configurando o Data Source

---
+++ src/test/java/com/algaworks/algafood/AlgafoodApplicationTests.java
@@ -0,0 +1,12 @@
+package com.algaworks.algafood;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
