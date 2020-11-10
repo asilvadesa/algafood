c9565507df15df168077c2b977671da301741974
Anderson de Sa <asilvadesa@gmail.com>
2020-11-09 22:04:54 -0400

    Atualizando um objeto no banco de dados

--- src/main/java/com/algaworks/algafood/jpa/AlterarCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/AlterarCozinhaMain.java
@@ -6,7 +6,7 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
-public class CadastroCozinhaMain {
+public class AlterarCozinhaMain {
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext =
                 new SpringApplicationBuilder(AlgafoodApplication.class)
@@ -15,14 +15,10 @@ public class CadastroCozinhaMain {
 
         CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 
-        Cozinha c1 = new Cozinha();
-        c1.setNome("Brasileira");
-
-        Cozinha c2 = new Cozinha();
-        c2.setNome("Japonesa");
-
-        cadastroCozinha.salvar(c1);
-        cadastroCozinha.salvar(c2);
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+        cozinha.setNome("Brasileira");
+        cadastroCozinha.salvar(cozinha);
 
     }
 }
