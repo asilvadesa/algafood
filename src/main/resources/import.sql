6bcc1c3a071b0d4a2acd594fcf1a40a6c55341f0
Anderson de Sa <asilvadesa@gmail.com>
2020-11-17 22:06:37 -0400

    Add collection resource de estados

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,3 +6,8 @@ insert into restaurante (nome, taxa_frete,cozinha_id) values ('JapaFood', 15.0,2
 insert into forma_pagamento (descricao) values ("Dinheiro")
 insert into forma_pagamento (descricao) values ("Cartao")
 insert into permissao (nome, descricao) values ("Produto","Permisao de leitura para objeto produto")
+insert into estado (nome) values ('Amazonas');
+insert into estado (nome) values ('Pará');
+insert into cidade (nome, estado_id) values ('Manaus', 1);
+insert into cidade (nome, estado_id) values ('Belém', 2);
+
