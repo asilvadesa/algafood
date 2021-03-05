2d25d669c932123a92d647e59737882be57a3fe7
Anderson de Sa <asilvadesa@gmail.com>
2021-03-05 12:01:53 -0400

    Add tratamento no metodo remover

--- src/main/java/com/algaworks/algafood/domain/service/RestauranteService.java
+++ src/main/java/com/algaworks/algafood/domain/service/RestauranteService.java
@@ -1,10 +1,11 @@
 package com.algaworks.algafood.domain.service;
 
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntindadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Restaurante;
-import com.algaworks.algafood.domain.respository.CozinhaRepository;
 import com.algaworks.algafood.domain.respository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +16,6 @@ public class RestauranteService {
 
     @Autowired
     private RestauranteRepository restauranteRepository;
-
-    @Autowired
-    private CozinhaRepository cozinhaRepository;
 
     public Restaurante salvar(Restaurante restaurante){
         Long cozinhaId = restaurante.getCozinha().getId();
@@ -33,6 +31,8 @@ public class RestauranteService {
             restauranteRepository.remover(restauranteId);
         }catch (EmptyResultDataAccessException exception){
             throw new EntindadeNaoEncontradaException(String.format("Não existe um cadastro de cozinha com código: %d", restauranteId));
+        }catch (DataIntegrityViolationException exception){
+            throw new EntidadeEmUsoException(exception.getMessage());
         }
     }
 }
